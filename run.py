cc512e5a3d938c42c37154086976f32fa3026e13
Paolo1207 <pamaui1207@gmail.com>
2025-05-18 15:52:07 +0800

    Initial commit

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from flask_migrate import Migrate
 from app import create_app, db
+import os
 
 app = create_app()
 migrate = Migrate(app, db)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
